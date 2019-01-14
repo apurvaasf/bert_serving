8e9d9c5ea876790acb6a401e9c3bbae27fcbfd0a
Apurva Bhandari <apurvaasf7@gmail.com>
2019-01-14 11:06:44 +0530

    Update train.sh

--- train.sh
+++ train.sh
@@ -1,15 +1,13 @@
 export BERT_BASE_DIR='uncased_L-12_H-768_A-12'
-python3 run_classifier.py \
-		--data_dir=data \
-		--task_name=chinese \
-		--vocab_file=$BERT_BASE_DIR/vocab.txt \
-		--bert_config_file=$BERT_BASE_DIR/bert_config.json \
-		--init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-		--max_seq_length=256 \
-		--train_batch_size=16 \
-		--learning_rate=2e-5 \
-		--num_train_epochs=30 \
-		--output_dir=chinese_model \
-		--use_tpu=False \
-		--do_train=true \
-		--do_eval=true
+python3 run_squad.py \	
+		--vocab_file=uncased_L-12_H-768_A-12/vocab.txt \
+		--bert_config_file=uncased_L-12_H-768_A-12/bert_config.json \
+		--init_checkpoint=uncased_L-12_H-768_A-12/bert_model.ckpt \
+		--do_train=True \
+		--train_file=./train-v1.1.json \
+		--train_batch_size=6 \
+		--learning_rate=3e-5 \
+		--num_train_epochs=2.0 \
+		--max_seq_length=384 \
+		--doc_stride=128 \
+		--output_dir=/tmp/squad_base/
