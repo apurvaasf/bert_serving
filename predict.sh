1f1033598cf0b1f530e4db71f8e9995c34cde0f3
Apurva Bhandari <apurvaasf7@gmail.com>
2019-01-14 12:06:35 +0530

    Update predict.sh

--- predict.sh
+++ predict.sh
@@ -1,14 +1,8 @@
 export BERT_BASE_DIR='chinese_L-12_H-768_A-12'
-python3 run_classifier.py \
-		--data_dir=data \
-		--task_name=chinese \
-		--vocab_file=$BERT_BASE_DIR/vocab.txt \
-		--bert_config_file=$BERT_BASE_DIR/bert_config.json \
-		--init_checkpoint=$BERT_BASE_DIR/bert_model.ckpt \
-		--max_seq_length=256 \
-		--train_batch_size=16 \
-		--learning_rate=2e-5 \
-		--num_train_epochs=30 \
-		--output_dir=chinese_model \
-		--use_tpu=False \
-		--do_predict=true
+python3 run_squad.py \
+		--vocab_file=uncased_L-12_H-768_A-12/vocab.txt \
+		--bert_config_file=uncased_L-12_H-768_A-12/bert_config.json \
+		--init_checkpoint=/tmp/squad_base/model.ckpt-29199 \
+		--do_predict=True \
+		--predict_file=./data.json \
+		--output_dir=/tmp/squad_base/output_folder/
